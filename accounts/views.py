9db80edfb1cd7c5329db37d9eeebb308750c6b4f
daniel-san17 <daniel.rotstein17@gmail.com>
2022-08-01 16:50:01 -0400

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+# UserCreationForm - creates a User (incl. username, password)
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Login
+from django.contrib.auth import login
+
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                print("Username or Password incorrect")
+
+    form = UserCreationForm()
+    context = {"form": form}
+
+    return render(request, "registration/signup.html", context)
