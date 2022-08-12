c02381270c81a66f83a6beaab07add4bb4213874
daniel-san17 <daniel.rotstein17@gmail.com>
2022-08-12 15:22:01 -0400

    edit scuba models

--- scuba_diving/models.py
+++ scuba_diving/models.py
@@ -8,6 +8,14 @@ class ScubaDiving(models.Model):
     equipment = models.CharField(max_length=150)
     description = models.TextField()
     image = models.URLField(null=True, blank=True)
+    image2 = models.URLField(null=True, blank=True)
+    image3 = models.URLField(null=True, blank=True)
+    image4 = models.URLField(null=True, blank=True)
+    image5 = models.URLField(null=True, blank=True)
+    languages = models.CharField(null=True, max_length=50)
+    currency = models.CharField(null=True, max_length=50)
+    timezone = models.CharField(null=True, max_length=50)
+    best_time = models.TextField(null=True)
 
     interest = models.ForeignKey(
         "interests.Interest",
