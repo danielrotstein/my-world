bfd3435fba10b78a9e97879bf0cb1e143f0c80f9
daniel-san17 <daniel.rotstein17@gmail.com>
2022-08-13 20:34:46 -0400

    finished the navbar

--- scuba_diving/urls.py
+++ scuba_diving/urls.py
@@ -12,7 +12,7 @@ from scuba_diving.views import (
 
 
 urlpatterns = [
-    path("interests/scuba", scuba_list_view, name="scuba_list"),
+    path("interests/scuba/", scuba_list_view, name="scuba_list"),
     path("interests/scuba/equipment", equipment_list_view,
          name="equipment_list"),
     path("interests/scuba/<int:pk>/", scuba_detail_view, name="scuba_detail"),
